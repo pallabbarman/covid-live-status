5e8fe2bb1b0779274b0eb8daac610b31145867e4
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-08 23:03:27 +0600

    country details and global info updated

--- js/script.js
+++ js/script.js
@@ -1,30 +1,45 @@
-// document.getElementById('search_button').addEventListener('click', () => {
-//     const inputCountry = document.getElementById('country').value;
-//     fetch('https://api.covid19api.com/summary')
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data);
-//             const country = data.country;
-//             document.getElementById('location').innerText = country;
-//             document.getElementById('totalConfirm').innerText = data.totalConfirm;
-//             document.getElementById('newConfirm').innerText = data.newConfirm;
-//             document.getElementById('totalDeath').innerText = data.totalDeath;
-//             document.getElementById('date').innerText = data.date;
-//         })
-//         .catch(error => alert("Please type a valid country name!"));
-// })
-document.getElementById('search_button').addEventListener('click', () => {
-    const inputCountry = document.getElementById('country').value;
-    fetch('https://api.covid19api.com/summary')
-        .then(res => res.json())
-        .then(data => {
-            const country = data.Countries[0].Country;
-            document.getElementById('location').innerText = "Country: " + country;
-            console.log(data.Countries[0].Country, data.Countries[0].TotalConfirmed, data.Countries[0].NewConfirmed, data.Countries[0].Date)
-            document.getElementById('totalConfirm').innerText = "Total Confirm: " + data.Countries[0].TotalConfirmed;
-            document.getElementById('newConfirm').innerText = "Total New Confirm: " + data.Countries[0].NewConfirmed;
-            document.getElementById('totalDeath').innerText = "Total Deaths: " + data.Countries[0].TotalDeaths;
-            document.getElementById('date').innerText = "Date: " + data.Countries[0].Date;
-        })
-        .catch(error => alert("Please type a valid country name!"));
-})
+// call api 
+fetch('https://api.covid19api.com/summary')
+    .then(res => res.json())
+    .then(data => displayGlobal(data.Global))
+    .catch(error => alert("Something Went wrong! Please try again later!"));
+
+// display global cases 
+const displayGlobal = global => {
+    const globalCase = document.getElementById('globalCase');
+    console.log(global.NewConfirmed);
+    globalCase.innerHTML = `
+    <h2>Total Case: ${global.TotalConfirmed}</h2>
+    <h2>Total Deaths: ${global.TotalDeaths}</h2>
+    <h2>Total Recovered: ${global.TotalRecovered}</h2>`;
+}
+
+// call api   critical
+fetch('https://disease.sh/v3/covid-19/countries')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+    .catch(error => alert("Something Went wrong! Please try again later!"));
+
+// details about countries     
+const displayCountries = countries => {
+    const countriesList = document.getElementById('countries');
+    countries.forEach(country => {
+        console.log(country.country);
+        const countryDetails = document.createElement('div');
+        countryDetails.className = 'country';
+        const countryInfo = `
+            <img src="${country.countryInfo.flag}"><br><br>
+            <h3 class="text-center">${country.country}</h3>
+            <h5 class="text-center">Continent: ${country.continent}</h5><br>
+            <p>Critical: ${country.critical}</p>
+            <p>Population: ${country.population}</p>
+            <p>New Confirmed: ${country.todayCases}</p>
+            <p>New Deaths: ${country.todayDeaths}</p>
+            <p>New Recovered: ${country.todayRecovered}</p>
+            <p>Total Confirmed: ${country.cases}</p>
+            <p>Total Deaths: ${country.deaths}</p>
+            <p>Total Recovered: ${country.recovered}</p>`;
+        countryDetails.innerHTML = countryInfo;
+        countriesList.appendChild(countryDetails);
+    });
+}
